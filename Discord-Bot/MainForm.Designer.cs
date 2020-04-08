b4943aba1c7d3274aa768030fd2ec8a601c01d54
Jason Filos <jason.filos@gmail.com>
2020-04-08 09:35:43 -0700

    adding first version in C#

---
+++ Discord-Bot/MainForm.Designer.cs
@@ -0,0 +1,87 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Jason
+ * Date: 4/7/2020
+ * Time: 10:42 PM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+namespace Discord_Bot
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Designer variable used to keep track of non-visual components.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+		
+		/// <summary>
+		/// Disposes resources used by the form.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing) {
+				if (components != null) {
+					components.Dispose();
+				}
+			}
+			base.Dispose(disposing);
+		}
+		
+		/// <summary>
+		/// This method is required for Windows Forms designer support.
+		/// Do not change the method contents inside the source code editor. The Forms designer might
+		/// not be able to load this method if it was changed manually.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.label1 = new System.Windows.Forms.Label();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.button1 = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// label1
+			// 
+			this.label1.Location = new System.Drawing.Point(119, 73);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(537, 116);
+			this.label1.TabIndex = 0;
+			this.label1.Text = "label1";
+			// 
+			// textBox1
+			// 
+			this.textBox1.Location = new System.Drawing.Point(119, 116);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(676, 26);
+			this.textBox1.TabIndex = 1;
+			// 
+			// button1
+			// 
+			this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 28F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.button1.Location = new System.Drawing.Point(134, 309);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(256, 105);
+			this.button1.TabIndex = 2;
+			this.button1.Text = "Start";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.Button1Click);
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(867, 601);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.textBox1);
+			this.Controls.Add(this.label1);
+			this.Name = "MainForm";
+			this.Text = "Discord-Bot";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+		}
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.Label label1;
+	}
+}
---
+++ Discord-Bot/MainForm.cs
@@ -0,0 +1,127 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Jason
+ * Date: 4/7/2020
+ * Time: 10:05 PM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace Discord_Bot
+{
+	
+	public partial class MainForm : Form
+	{
+		
+		public MainForm()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+			for (int i = 0; i < 50; i++) 
+			{
+				/*
+				Step1 : pls fish
+				 */
+				
+				double randNormal = generateGaussian(50,6);
+				int t1 = 1000*(int)randNormal;
+				
+				Thread.Sleep(t1);
+				SendKeys.SendWait("pls fish~");
+				
+				/*
+				Step2 : pls beg
+				 */			
+				
+				double randNormal2 = generateGaussian(20,4);
+				int t2 = 1000*(int)randNormal2;
+					
+				Thread.Sleep(t2);
+				SendKeys.SendWait("pls beg~");
+
+				/*
+				Step3 : pls trivia
+				 */			
+				
+				double randNormal3 = generateGaussian(40,5);
+				int t3 = 1000*(int)randNormal3;
+					
+				Thread.Sleep(t3);
+				SendKeys.SendWait("pls trivia~");
+			
+				/*
+				Step3.5 : answer trivia (a/b/c/d)
+				 */		
+				
+				double randNormal4 = generateGaussian(5,2);
+				int t4 = 1000*(int)randNormal4;
+				
+				Thread.Sleep(t4);
+				Random randtest = new Random();
+				double test = randtest.NextDouble();
+				if (test >= 0 & test < 0.25)
+				{
+					SendKeys.SendWait("a~");
+				}
+				else if (test >= 0.25 & test < 0.5)
+				{
+					SendKeys.SendWait("b~");
+				}
+				else if (test >= 0.5 & test < 0.75)
+				{
+					SendKeys.SendWait("c~");
+				}
+				else if (test >= 0.75 & test < 1.0)
+				{
+					SendKeys.SendWait("d~");
+				}
+				else
+				{
+					SendKeys.SendWait("a~");
+				}
+				
+				/*
+				Step4 : pls beg
+				 */		
+				
+				double randNormal5 = generateGaussian(20,4);
+				int t5 = 1000*(int)randNormal5;
+					
+				Thread.Sleep(t5);
+				SendKeys.SendWait("pls beg~");			
+			}
+			
+		}
+		public double generateGaussian(int mu, int sigma)
+		{
+			Random rand = new Random();
+			double u1 = 1.0-rand.NextDouble(); //uniform(0,1] random doubles
+			double u2 = 1.0-rand.NextDouble();
+			double randStdNormal = Math.Sqrt(-2.0 * Math.Log(u1)) * Math.Sin(2.0 * Math.PI * u2); //random normal(0,1)
+			return( mu + sigma * randStdNormal); //random normal(mean,stdDev^2)
+		}
+
+		void Button1Click(object sender, EventArgs e)
+		{
+			//double test = generateGaussian(0,1);
+			//label1.Text = test.ToString();
+//			//label1.Text=textBox1.Text;
+//			if (test < 0.5 & test > 0.25)
+//			{
+//				label1.Text = test.ToString();
+//			}
+
+		}
+	}	
+}
+
+	
+	
+	
